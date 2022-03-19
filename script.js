6b9af16c06d09c0510819972de576080b7d7cb89
minabakhtiari <bakhtiari.m2345@gmail.com>
2022-03-19 14:01:53 +0330

    added calculation

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("calculation");
+function add(){
+    var n1=document.getElementById("t1").value ;
+    var n2=document.getElementById("t2").value ;
+    document.getElementById("t3").value=Number(n1)+Number(n2);
+
+}
+
+function sub(){
+    var n1=document.getElementById("t1").value ;
+    var n2=document.getElementById("t2").value ;
+    document.getElementById("t3").value=Number(n1)-Number(n2);
+
+}
+function mul(){
+    var n1=document.getElementById("t1").value ;
+    var n2=document.getElementById("t2").value ;
+    document.getElementById("t3").value=Number(n1)*Number(n2);
+
+}
+function div(){
+    var n1=document.getElementById("t1").value ;
+    var n2=document.getElementById("t2").value ;
+    document.getElementById("t3").value=Number(n1)/Number(n2);
+
+}
